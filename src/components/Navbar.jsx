b547275055be6850723453727319339a5f75b2cf
ronak5487 <20bee055@nith.ac.in>
2023-06-24 14:13:07 +0530

    change

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -24,7 +24,7 @@ const Navbar = () => {
 <NavbarItem key={item + index} title={item} />
 
         ))}
-        <li className="bg-[#2952e3] py-2 px-7 mx-4 rounded-full cursor-pointer hover:bg-[#2546bd]">Login</li>
+        
       </ul>
       <div className="flex relative">
         {toggleMenu &&(
